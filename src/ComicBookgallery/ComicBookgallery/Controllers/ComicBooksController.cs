64c39c45b4f00249ea374627a8e972adb1d6606c
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2021-11-25 17:22:25 +0100

    comi book controller added

---
+++ src/ComicBookgallery/ComicBookgallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ComicBookgallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+    }
+}
